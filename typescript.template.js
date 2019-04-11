a92b1f1e5acdbe6a7a09e30b24f83225b0489f86
Tomeu Cabot <cabotvalloritomeu@gmail.com>
2019-04-11 21:03:29 +0200

    inital commit

---
+++ typescript.template.js
@@ -0,0 +1,12 @@
+function template(
+  { template },
+  opts,
+  { imports, componentName, props, jsx, exports }
+) {
+  const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+  return typeScriptTpl.ast`
+      import * as React from 'react';
+      export const ${componentName} = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
+    `;
+}
+module.exports = template;
